0c618b23a8a512752ec0a02b15091bd8cadf6970
Oyinade003 <Opedamola003@gmail.com>
2024-03-29 03:33:41 +0100

    Maincontent redesign

--- product_page/src/components/MainContent.jsx
+++ product_page/src/components/MainContent.jsx
@@ -10,46 +10,72 @@ import skincareIcon from '../assets/skincare.jpg';
 import haircareIcon from '../assets/haircare.png';
 import gadgetsIcon from '../assets/gadgets.png';
 import stationeriesIcon from '../assets/stationeries.png';
+import dflogo from '../assets/dflogo.png';
+import star from '../assets/Star 1.png';
 
 const MainContent = () => {
   return (
     <div className={styles.mainContent}>
       <div className={styles.categoryIcons}>
-        <div className={styles.categoryIcon}>
+      <div className={`${styles.categoryIcon1} ${styles.categoryIcon}`}>
           <img src={foodIcon} alt="Food" />
           <h3>Food</h3>
         </div>
-        <div className={styles.categoryIcon}>
+        <div className={`${styles.categoryIcon3} ${styles.categoryIcon}`}>
           <img src={clothesIcon} alt="Clothes" />
           <h3>Clothes</h3>
         </div>
-        <div className={styles.categoryIcon}>
+        <div className={`${styles.categoryIcon} ${styles.categoryIcon4}`}>
           <img src={booksIcon} alt="Books" />
           <h3>Books</h3>
         </div>
-        <div className={styles.categoryIcon}>
+        <div className={`${styles.categoryIcon} ${styles.categoryIcon2}`}>
           <img src={footwearIcon} alt="Footwear" />
           <h3>Footwear</h3>
         </div>
-        <div className={styles.categoryIcon}>
+        <div className={`${styles.categoryIcon} ${styles.categoryIcon3}`}>
           <img src={skincareIcon} alt="Skincare" />
           <h3>Skincare</h3>
         </div>
-        <div className={styles.categoryIcon}>
+        <div className={`${styles.categoryIcon1} ${styles.categoryIcon}`}>
           <img src={haircareIcon} alt="Haircare" />
           <h3>Haircare</h3>
         </div>
-        <div className={styles.categoryIcon}>
+        <div className={`${styles.categoryIcon} ${styles.categoryIcon4}`}>
           <img src={gadgetsIcon} alt="Gadgets" />
           <h3>Gadgets</h3>
         </div>
-        <div className={styles.categoryIcon}> 
+        <div className={`${styles.categoryIcon} ${styles.categoryIcon2}`}>
           <img src={stationeriesIcon} alt="Stationeries" />
           <h3>Stationeries</h3>
         </div>
       </div>
+
+      {/* VENDORS */}
+      <div className={styles.vendors}>
+        <h2> Shop from a Vendor near you 🛒 </h2>
+        <div className={styles.vendor}>
+          <div className={styles.vendorheader}>
+            <p><b> Dummy Foods </b> </p>
+            <p> Food For All </p>
+            <img src={dflogo} alt="dflogo" className={styles.dflogo} />
+          </div>
+          <h4 className={styles.contenthead}> Dummy Foods</h4>
+          <ul className={styles.product}>
+            <li>&#x1F375; Rice </li>
+            <li>&#x1F375;  Stirfry </li>
+            <li>&#x1F375; Pizza Slice </li>
+          </ul>
+          <p> <b> Price Range:</b> &#8358;500 - &#8358;6,000 </p>
+          <div className={styles.rating}>
+            <img src={star} alt='star' className={styles.star}/> <p><b>5.0</b>(20)</p>
+          </div>
+          <p>🗺️ UI, Awo Hall </p>
+          <p><small> Delivery Charges Apply </small></p>
+        </div>
+      </div>
     
-      {/* <div className={styles.vendor}>
+      {/* <div className={styles.vendorSignUp}>
         <button className={styles.vendorButton}>Get started as a Vendor</button>
       </div> */}
     </div>
