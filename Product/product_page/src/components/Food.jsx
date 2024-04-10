12fe77f4aeddaef3b46229ee2cc369652bf97583
Oyinade003 <Opedamola003@gmail.com>
2024-04-10 08:52:10 +0100

    viewmore

--- Product/product_page/src/components/Food.jsx
+++ Product/product_page/src/components/Food.jsx
@@ -1,14 +1,86 @@
+import React, { useRef } from "react";
+import styles from "../styles/Food.module.css";
+
+import star from "../assets/Star 1.png";
+import cake from '../assets/cake.png';
+import pizza from "../assets/pizza.png";
+import sharwama from "../assets/sharwama.png";
+import jollofRice from "../assets/products-food.png";
+
+const foodData = [
+  {
+    foodImage: cake,
+    foodName: "Red Velvet",
+    price: "₦2,500",
+    rating: 4.2,
+    deliveryNote: 'Delivery Charges Apply',
+    deliveryTime: "15-20 minutes",
+
+  },
+  {
+    foodImage: pizza,
+    foodName: "Pepperonni Pizza",
+    price: "₦4,000",
+    rating: 3.00,
+    deliveryTime: "15-20 minutes",
+    deliveryNote: 'Delivery Charges Apply',
+  },
+  {
+    foodImage: sharwama,
+    foodName: "Sharwama",
+    price: "₦3,200",
+    rating: 4.00,
+    deliveryTime: "5-15 minutes",
+    deliveryNote: 'Free Delivery',
+  },
+  {
+    foodImage: jollofRice,
+    foodName: "Jollof Rice",
+    price: "₦2,000",
+    rating: 2.4,
+    deliveryTime: "5 minutes",
+    deliveryNote: 'Free Delivery',
+  },
+  // Add more vendor data objects here
+];
+
+const FoodCard = ({ foodImage, foodName, price, rating, deliveryTime, deliveryNote }) => {
+  return (
+    <div className={styles.FoodCard}>
+      <a href="/">
+      <div className={styles.foods}>
+        <img src={foodImage} alt={`${foodName} image`} className={styles.foodImage} />
+        <div className={styles.foodcontent}>
+          <p className={styles.foodName}> <b>{foodName}</b></p>
+          <p> {price}</p>
+          <p> {deliveryTime} </p>
+          <div className={styles.deliveryData}>
+            <p>{deliveryNote}</p>
+            <div className={styles.rating}>
+              <p>{rating}</p>
+              <img src={star} className={styles.star} />
+            </div>
+          </div>
+        </div>
+      </div>
+      </a>
+    </div>
+  );
+};
+
 const FoodList = () => {
   const foodListRef = useRef(null);
+
   const handleScroll = (direction) => {
     const containerWidth = foodListRef.current.offsetWidth;
-    const scrollAmount = direction === "left" ? -containerWidth : containerWidth;
+    const scrollAmount =
+      direction === "left" ? -containerWidth : containerWidth;
     foodListRef.current.scrollBy({ left: scrollAmount, behavior: "smooth" });
   };
 
   return (
     <div className={styles.foodListContainer}>
-      <h2>Foods and Snacks</h2>
+      <h2> Foods and Snacks </h2>
       <div className={styles.scrollContainer}>
         <button
           className={`${styles.scrollButton} ${styles.leftButton}`}
@@ -44,3 +116,5 @@ const FoodList = () => {
     </div>
   );
 };
+
+export default FoodList;
