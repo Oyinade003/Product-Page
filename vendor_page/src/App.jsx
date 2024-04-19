b47d9a03e0cf448f795ac51db31a70910de9f3d9
Oyinade003 <Opedamola003@gmail.com>
2024-04-19 02:14:44 +0100

    vendor page

---
+++ vendor_page/src/App.jsx
@@ -0,0 +1,216 @@
+import unilogo from './assets/newlogo.png'
+import adoylogo from './assets/adoylogo.jpg'
+import bgimage from './assets/vendorcart.png'
+import dflogo from './assets/dflogo.png'
+import star from './assets/Star 1.png'
+import './App.css'
+
+function App() {
+  return (
+    <div className='container'>
+      <header>
+        <img src={unilogo} alt='logo' className='unilogo' />
+        <h2> UNICLIQUE </h2>
+      </header>
+      <div className='bgimage'>
+
+      </div>
+      <div className='maincontent'>
+        <a href='./index.html' className='vendor'>
+          <div className='vendor-header'>
+            <p><b> Dummy Foods </b> </p>
+            <p> Food For All </p>
+            <img src={dflogo} alt="dflogo" className='dflogo' />
+          </div>
+          <h4 className='content-head'> Dummy Foods</h4>
+          <ul className='product'>
+            <li>&#x1F375; Rice </li>
+            <li>&#x1F375;  Stirfry </li>
+            <li>&#x1F375; Pizza Slice </li>
+          </ul>
+          <p> <b> Price Range:</b> &#8358;500 - &#8358;6,000 </p>
+          <div className='rating'>
+            <img src={star} alt='star' className='star'/> <p><b>5.0</b>(20)</p>
+          </div>
+          <i class="fa fa-map-marker" aria-hidden="true"> UI, Awo Hall </i>
+          <p><small> Delivery Charges Apply </small></p>
+        </a>
+
+        <a href='/.index.html' className='vendor sub-vendor'>
+          <div className='vendor-header'>
+            <p><b> Dummy Foods </b> </p>
+            <p> Food For All </p>
+            <img src={dflogo} alt="dflogo" className='dflogo' />
+          </div>
+          <h4 className='content-head'> Dummy Foods</h4>
+          <ul className='product'>
+            <li>&#x1F375; Rice </li>
+            <li>&#x1F375;  Stirfry </li>
+            <li>&#x1F375; Pizza Slice </li>
+          </ul>
+          <p> <b> Price Range:</b> &#8358;500 - &#8358;6,000 </p>
+          <div className='rating'>
+            <img src={star} alt='star' className='star'/> <p><b>5.0</b>(20)</p>
+          </div>
+          <i class="fa fa-map-marker" aria-hidden="true"> UI, Awo Hall </i>
+          <p><small> Delivery Charges Apply </small></p>
+        </a>
+      </div>
+
+      <div className='maincontent'>
+      <a href='./index.html' className='vendor'>
+          <div className='vendor-header'>
+            <p><b> Dummy Foods </b> </p>
+            <p> Food For All </p>
+            <img src={dflogo} alt="dflogo" className='dflogo' />
+          </div>
+          <h4 className='content-head'> Dummy Foods</h4>
+          <ul className='product'>
+            <li>&#x1F375; Rice </li>
+            <li>&#x1F375;  Stirfry </li>
+            <li>&#x1F375; Pizza Slice </li>
+          </ul>
+          <p> <b> Price Range:</b> &#8358;500 - &#8358;6,000 </p>
+          <div className='rating'>
+            <img src={star} alt='star' className='star'/> <p><b>5.0</b>(20)</p>
+          </div>
+          <i class="fa fa-map-marker" aria-hidden="true"> UI, Awo Hall </i>
+          <p><small> Delivery Charges Apply </small></p>
+        </a>
+
+        <a href='./index.html' className='vendor sub-vendor'>
+          <div className='vendor-header'>
+            <p><b> Dummy Foods </b> </p>
+            <p> Food For All </p>
+            <img src={dflogo} alt="dflogo" className='dflogo' />
+          </div>
+          <h4 className='content-head'> Dummy Foods</h4>
+          <ul className='product'>
+            <li>&#x1F375; Rice </li>
+            <li>&#x1F375;  Stirfry </li>
+            <li>&#x1F375; Pizza Slice </li>
+          </ul>
+          <p> <b> Price Range:</b> &#8358;500 - &#8358;6,000 </p>
+          <div className='rating'>
+            <img src={star} alt='star' className='star'/> <p><b>5.0</b>(20)</p>
+          </div>
+          <i class="fa fa-map-marker" aria-hidden="true"> UI, Awo Hall </i>
+          <p><small> Delivery Charges Apply </small></p>
+        </a>
+      </div>
+
+      <div className='maincontent'>
+      <a href='./index.html'className='vendor'>
+          <div className='vendor-header'>
+            <p><b> Dummy Foods </b> </p>
+            <p> Food For All </p>
+            <img src={dflogo} alt="dflogo" className='dflogo' />
+          </div>
+          <h4 className='content-head'> Dummy Foods</h4>
+          <ul className='product'>
+            <li>&#x1F375; Rice </li>
+            <li>&#x1F375;  Stirfry </li>
+            <li>&#x1F375; Pizza Slice </li>
+          </ul>
+          <p> <b> Price Range:</b> &#8358;500 - &#8358;6,000 </p>
+          <div className='rating'>
+            <img src={star} alt='star' className='star'/> <p><b>5.0</b>(20)</p>
+          </div>
+          <i class="fa fa-map-marker" aria-hidden="true"> UI, Awo Hall </i>
+          <p><small> Delivery Charges Apply </small></p>
+        </a>
+
+        <a href='./index.html' className='vendor sub-vendor'>
+        <div className='vendor-header'>
+            <p><b> Dummy Foods </b> </p>
+            <p> Food For All </p>
+            <img src={dflogo} alt="dflogo" className='dflogo' />
+          </div>
+          <h4 className='content-head'> Dummy Foods</h4>
+          <ul className='product'>
+            <li>&#x1F375; Rice </li>
+            <li>&#x1F375;  Stirfry </li>
+            <li>&#x1F375; Pizza Slice </li>
+          </ul>
+          <p> <b> Price Range:</b> &#8358;500 - &#8358;6,000 </p>
+          <div className='rating'>
+            <img src={star} alt='star' className='star'/> <p><b>5.0</b>(20)</p>
+          </div>
+          <i class="fa fa-map-marker" aria-hidden="true"> UI, Awo Hall </i>
+          <p><small> Delivery Charges Apply </small></p>
+        </a>
+      </div>
+
+      <div className='maincontent'>
+      <a href='./index.html' className='vendor'>
+          <div className='vendor-header'>
+            <p><b> Dummy Foods </b> </p>
+            <p> Food For All </p>
+            <img src={dflogo} alt="dflogo" className='dflogo' />
+          </div>
+          <h4 className='content-head'> Dummy Foods</h4>
+          <ul className='product'>
+            <li>&#x1F375; Rice </li>
+            <li>&#x1F375;  Stirfry </li>
+            <li>&#x1F375; Pizza Slice </li>
+          </ul>
+          <p> <b> Price Range:</b> &#8358;500 - &#8358;6,000 </p>
+          <div className='rating'>
+            <img src={star} alt='star' className='star'/> <p><b>5.0</b>(20)</p>
+          </div>
+          <i class="fa fa-map-marker" aria-hidden="true"> UI, Awo Hall </i>
+          <p><small> Delivery Charges Apply </small></p>
+        </a>
+
+        <a href='./index.html' className='vendor sub-vendor'>
+          <div className='vendor-header'>
+            <p><b> Dummy Foods </b> </p>
+            <p> Food For All </p>
+            <img src={dflogo} alt="dflogo" className='dflogo' />
+          </div>
+          <h4 className='content-head'> Dummy Foods</h4>
+          <ul className='product'>
+            <li>&#x1F375; Rice </li>
+            <li>&#x1F375;  Stirfry </li>
+            <li>&#x1F375; Pizza Slice </li>
+          </ul>
+          <p> <b> Price Range:</b> &#8358;500 - &#8358;6,000 </p>
+          <div className='rating'>
+            <img src={star} alt='star' className='star'/> <p><b>5.0</b>(20)</p>
+          </div>
+          <i class="fa fa-map-marker" aria-hidden="true"> UI, Awo Hall </i>
+          <p><small> Delivery Charges Apply </small></p>
+        </a>
+      </div>
+
+      <button className="signup-div"> <a href="../SignUp Page/index.html"> Get Started as a Seller </a></button>
+
+      <footer>
+      <div className="footer-div">
+        <div className="footer-div-content">
+            <h3> Uniclique </h3>
+            <a href="./index.html"> About Us </a>
+            <a href="./index.html"> FAQs </a>
+            <a href="./index.html"> Contact Us </a>
+        </div>
+        <div className="footer-div-content">
+            <h4> Account </h4>
+            <a href="./index.html"> Seller Login </a>
+            <a href="./index.html"> Sign Up </a>
+            <a href="./index.html"> My Orders </a>
+        </div>
+        <div className="footer-div-content">
+            <h4> Social </h4>
+            <a href="./index.html"> Instagram </a>
+            <a href="./index.html"> Twitter </a>
+            <a href="./index.html"> LinkedIn </a>
+        </div>
+    </div>
+
+        <p class="copyright"> &copy; 2023 All Rights Reserved, Uniclique</p>
+      </footer>
+    </div>
+  )
+}
+
+export default App
